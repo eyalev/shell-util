dc5870816db77e065d5f06deda85ac89ea43160e
Eyal Levin <eyalev@gmail.com>
2020-06-20 10:49:11 +0300

    Update

--- shell_util/shell.py
+++ shell_util/shell.py
@@ -1,6 +1,7 @@
 
 import subprocess
 import shlex
+from subprocess import check_output, STDOUT, CalledProcessError
 
 from shell_util.shell_result import ShellResult
 
@@ -16,9 +17,21 @@ def run_command(command, stdout=None, in_dir=None):
 def run_command_and_get_shell_result(command):
 
     command_split = shlex.split(command)
-    status, output = subprocess.getstatusoutput(command_split)
+    status, output = _get_status_and_output(command_split)
     shell_result = ShellResult(status, output, command)
 
     return shell_result
 
+
+def _get_status_and_output(command_split):
+
+    try:
+        data = check_output(command_split, universal_newlines=True, stderr=STDOUT)
+        exitcode = 0
+    except CalledProcessError as ex:
+        data = ex.output
+        exitcode = ex.returncode
+    if data[-1:] == '\n':
+        data = data[:-1]
+    return exitcode, data
 
